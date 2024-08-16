9d73955e63242c1dda34d7e3e41a62c58015be87
root <fikadugetnet428@gmail.com>
2024-08-16 06:54:34 -0700

    added getcatagories and create project

--- controllers/projectcontrollers.js
+++ controllers/projectcontrollers.js
@@ -24,3 +24,20 @@ exports.newproject = async(req,res) =>
 
   res.render('addproject',{tags});
 }
+exports.createProject = async (req, res) => {
+    const checkedOptions = req.body.myCheckboxes || [];
+    const projectDetails = req.body;
+    try {
+      await projectService.createProject(projectDetails, req.user._id,checkedOptions);
+      res.redirect('/projects/myprojects');
+    } catch (error) {
+      console.error('Error creating project:', error);
+      res.status(500).send('Internal Server Error');
+    }
+
+};
+exports.getcatagories = async(req,res) =>
+  {
+    const tags = await projectService.gettags();
+    res.render("projectcatagories",{tags})
+  }
