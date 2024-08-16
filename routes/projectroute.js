637c479d01a391ab16e7e134fd30a2110fac1bb6
root <fikadugetnet428@gmail.com>
2024-08-16 06:52:17 -0700

    added project catagories and search project route

--- routes/projectroute.js
+++ routes/projectroute.js
@@ -4,4 +4,11 @@ const express = require('express');
 router.get('/',ensureAuthenticated, getProjects);
 router.get('/addproject', ensureAuthenticated,newproject);
 router.get("/mycollabration",ensureAuthenticated,getmycollabration)
+router.post('/addproject', ensureAuthenticated,createProject);
+router.get("/projectcatagories",ensureAuthenticated,getcatagories);
+router.get('/searchproject', ensureAuthenticated,searchProjects);
 
+
+
+
+module.exports = router;
