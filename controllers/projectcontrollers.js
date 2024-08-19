37b41a8b32bc0b48c181b5bec90f09e52b05c349
zelalem3 <zgetnet24@example.com>
2024-08-19 04:28:14 -0700

    implemented flashing feature to the project route

--- controllers/projectcontrollers.js
+++ controllers/projectcontrollers.js
@@ -1,46 +1,58 @@
-const ProjectTag = require('../models/projecttag');
-const projectService = require('../services/projectservice');
+const projectservice = require("../services/projectservice");
+const flash = require('connect-flash');
+const userService = require('../services/userservice');
 
 
-exports.getProjects = async (req, res) => {
 
-    try {
-      const projects = await projectService.getProjects(req.user._id);
-      res.render('project', { projects, messages: {
-        success: req.flash('success'),
-        error: req.flash('error')
-      } });
-    } catch (error) {
-      console.error('Error fetching projects:', error);
-      res.status(500).send('Internal Server Error');
-    }
 
-};
+
+exports.acceptinterest = async(req,res) =>{
+  const projectid = req.params.projectid;
+  const userid = req.params.userid;
+  const acceptinterest = projectservice.acceptinterest();
+  if (acceptinterest)
+  {
+    res.status(200).json({"message":'added to the team'});
+  }
+  else{
+    res.status(500).send('Internal Server Error');
+  }
+}
+
 
 
 exports.newproject = async(req,res) =>
-{
-  const tags = await projectService.gettags();
+    {
+      const tags = await projectservice.gettags();
+      const currentuser =  userService.getUserById(req.user._id);
+      res.render('addproject',{
+        tags,
+        
+       messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
 
-  res.render('addproject',{tags});
-}
-exports.createProject = async (req, res) => {
-    const checkedOptions = req.body.myCheckboxes || [];
-    const projectDetails = req.body;
-    try {
-      await projectService.createProject(projectDetails, req.user._id,checkedOptions);
-      res.redirect('/projects/myprojects');
-    } catch (error) {
-      console.error('Error creating project:', error);
-      res.status(500).send('Internal Server Error');
+    });
     }
-
-};
+    exports.createProject = async (req, res) => {
+        const checkedOptions = req.body.myCheckboxes || [];
+        const projectDetails = req.body;
+        try {
+          await projectservice.createProject(projectDetails, req.user._id,checkedOptions);
+          res.redirect('/projects/myprojects');
+        } catch (error) {
+          console.error('Error creating project:', error);
+          res.status(500).send('Internal Server Error');
+        }
+    
+    };
 exports.getcatagories = async(req,res) =>
-  {
-    const tags = await projectService.gettags();
-    res.render("projectcatagories",{tags})
-  }
+      {
+        const tags = await projectService.gettags();
+        const currentuser =  userService.getUserById(req.user._id);
+        res.render("projectcatagories",{tags,currentuser})
+      }
 
 exports.getsearchresult = async(req,res) =>
 {
@@ -50,8 +62,15 @@ exports.getsearchresult = async(req,res) =>
 exports.getProjects = async(req,res) =>
 {
 
-    const projects = projectservice.getProjects(req.user._id)
-    res.render("project",{projects})
+    const projects = projectservice.getProjects(req.user._id);
+    const currentuser =  userService.getUserById(req.user._id);
+    res.render("project",{
+        projects,
+        messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
+})
 }
 
 
@@ -60,23 +79,36 @@ exports.getMyProjects = async(req,res) =>
 
     const id = req.user._id;
     const project = await projectservice.getMyProject(id);
+    const currentuser =  userService.getUserById(req.user._id);
 
 
 
-
-    res.render("myproject",{project})
+    res.render("myproject",{
+        project,
+       messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
+     })
 
 
 
 }
 
+
+
 exports.getmycollabration = async(req,res) =>
 {
     const id = req.user._id;
     const projects = projectservice.getcollabration(id)
+    const currentuser =  userService.getUserById(req.user._id);
 
-
-    res.render("mycollabration",{projects})
+    res.render("mycollabration",{
+        projects,
+       messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser })
 }
 
 
@@ -100,9 +132,14 @@ exports.getProjectById = async(req,res) =>
 
 {
     const project = await projectservice.getProjectById(req.params.id);
+    const currentuser =  userService.getUserById(req.user._id);
 
-
-    res.render("specficproject",{project})
+    res.render("specficproject",{
+        project,
+        messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser })
 }
 
 
@@ -110,21 +147,41 @@ exports.getProjects = async(req,res) =>
 {
 
 
-    const projects = await projectservice.getProjects()
-    res.render("project",{projects})
+    const projects = await projectservice.getProjects();
+    const currentuser =  userService.getUserById(req.user._id);
+
+    res.render("project",{projects,
+        messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
+    })
 
 }
 
 exports.getmyprojectwithid = async(req,res) =>
 {
     const project = await projectservice.getMyProjectbyid(req.params.id);
-   res.render("interestinmyproject",{project})
+    const currentuser =  userService.getUserById(req.user._id);
+
+   res.render("interestinmyproject",{project,
+        messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
+   })
 }
 exports.getFollowingProjects = async(req,res) =>
 {
-    const projects = await projectservice.getfollowingprojects(req.user._id)
+    const projects = await projectservice.getfollowingprojects(req.user._id);
+    const currentuser =  userService.getUserById(req.user._id);
 
-    res.render("projects",{projects})
+    res.render("projects",{projects,
+      messages: {
+        success: req.flash('success'),
+        error: req.flash('error')
+      },currentuser
+    })
 
 }
 
@@ -134,16 +191,20 @@ exports.deleteproject = async(req,res) =>
     const del = projectservice.deleteproject(id);
     if(del)
     {
-        req.flash("successfully delted the project")
+     
+        req.flash('success', 'successfully deleted the project');
         res.redirect("/projects/myprojects")
     }
     else{
-        req.flash("project deletion not successful")
+  
+        req.flash('error', 'project deletion not successful');
         res.redirect("/projects/myprojects")
     }
-
+    
 
 }
+
+
 exports.registerInterestwithid = async(req,res) =>
 {
     const projectId=req.params.id;
@@ -151,13 +212,11 @@ exports.registerInterestwithid = async(req,res) =>
     const project = await projectservice.registerInterest(projectId, userid);
     if(project)
     {
-        req.flash("interest successfully registered")
+      req.flash('success', 'Interest successfully registered');
         
 
     }
-    else{
-        req.flash("interest already registered")
-
+    else{req.flash('error', 'Interest already registered');
     }
     res.redirect("/projects");
 }
