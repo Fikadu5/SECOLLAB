368fae461f2dff49c6057461849e2736ade00770
root <fikadugetnet428@gmail.com>
2024-08-21 13:54:36 -0700

    add more test to the blog service to my blogs service

--- test/blogService.test.js
+++ test/blogService.test.js
@@ -1,14 +1,26 @@
-const blogService = require('../services/blogservice'); // Your service
-const Blog = require('../models/blog'); // Your Blog model
 
-jest.mock('../models/Blog'); // Mock Blog model
+    const { getMyBlogs,checklike,newtag,gettags,getcatblogs } = require('../services/blogservice');
+    const Blog = require('../models/blog');
+    const BlogTag = require('../models/Blogtag')
+    const mongoose = require('mongoose');
 
-describe('blogService', () => {
-  it('should call Blog.find() to get all blogs', async () => {
-    await blogService.getAllBlogs();
-    expect(Blog.find).toHaveBeenCalled();
-  });
+    jest.mock('../models/blog');
 
-  // Add more tests for specific service functions
-  // ...
-});
+    it('should fetch blogs successfully when given a valid user ID', async () => {
+      const mockBlogs = [
+        { _id: new mongoose.Types.ObjectId(), title: 'Blog 1', owner: 'user1' },
+        { _id: new mongoose.Types.ObjectId(), title: 'Blog 2', owner: 'user1' }
+      ];
+
+      Blog.find.mockReturnValue({
+        populate: jest.fn().mockReturnThis(),
+        skip: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        exec: jest.fn().mockResolvedValue(mockBlogs)
+      });
+
+      const result = await getMyBlogs('user1', 1, 10);
+
+      expect(result).toEqual(mockBlogs);
+      expect(Blog.find).toHaveBeenCalledWith({ owner: 'user1' });
+    });
