e1a1c7b2fa07ee5d26837c4893e14af562f964d6
zelalem3 <zgetnet24@example.com>
2024-08-22 11:26:44 -0700

    added javascript for searching capabilities on the front end

---
+++ public/js/allblog.js
@@ -0,0 +1,33 @@
+
+
+var search = document.getElementById("search");
+
+
+function change()
+{
+
+event.preventDefault();
+var text = document.getElementById("text").value;
+if (text == "")
+{
+event.preventDefault();
+}
+else{
+
+ window.location.href = "localhost:3000/blogs/searchblog/" + text;
+
+}
+
+
+
+
+}
+
+search.addEventListener("click", change);
+var text = document.getElementById("text");
+text.addEventListener("keydown", call);
+function call()
+{
+if (event.key === "Enter") {
+    change();
+  }}
