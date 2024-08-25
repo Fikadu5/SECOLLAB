2e5ae16f3f60d9cee5f25dacb8aae541ccf3de6e
zelalem3 <zgetnet24@example.com>
2024-08-25 03:17:43 -0700

    added create index function for the blog and project collections

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
-
+const flash = require('connect-flash');
+const path = require('path');
+const dotenv = require('dotenv');
 const userRoutes = require('./routes/userroutes');
+const blogRoutes = require('./routes/blogroutes');
 const authRoutes = require('./routes/authroute');
+const projectRoutes = require('./routes/projectroute');
+const { User } = require('./models/user');
+const cors = require('cors');
+const Project = require('./models/project');
+const Blog= require('./models/blog');
+const rateLimit = require('express-rate-limit');
+const { ensureAuthenticated } = require('./middleware/authMiddleware');
 
 const app = express();
 dotenv.config();
@@ -15,6 +25,17 @@ app.use(cors());
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/zelalem", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
+  .then(async () => {
+    await Project.collection.createIndex({ title: 'text' });
+    console.log('Text index created on title');
+  })
+  .then(() => console.log('MongoDB connected'))
+  .then(async () => {
+    await Blog.collection.createIndex({ title: 'text' });
+    console.log('Text index created on title');
+  })
+  
+  
   .catch(err => console.log('MongoDB connection error:', err));
 
 // Middleware
@@ -34,40 +55,49 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    // httpOnly: true, 
+    // secure: true 
   }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
 
 // Passport middleware
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Logging Middleware for Debugging
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  console.log("Session info:", req.session);
-  console.log("User info:", req.user);
-  next();
-});
 
-
-// Logging Middleware for Debugging
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  console.log("Session info:", req.session);
-  console.log("User info:", req.user);
-  next();
-});
+/* Logging Middleware for Debugging
+// app.use((req, res, next) => {
+//   console.log(`${req.method} ${req.url}`);
+//   console.log("Session info:", req.session);
+//   console.log("User info:", req.user);
+//   next();
+// });
+*/
 
 
 // Routes
 app.use('/', userRoutes);
 app.use('/authenticate', authRoutes);
+app.use('/blogs', blogRoutes);
+app.use('/projects', projectRoutes);
+
+
+
+
+
+
+
 
 
 
