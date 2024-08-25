284c87eb7910b9645e6092a652246442c0952ade
root <fikadugetnet428@gmail.com>
2024-08-25 02:55:56 -0700

    fix: search route fixed for projects

--- routes/projectroute.js
+++ routes/projectroute.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const { getProjects, createProject, getProjectById, searchProjects, getmyprojectwithid, 
     getsearchresult,searchResults,getFollowingProjects ,getMyProjects,getmycollabration,deleteproject,newproject  
-  ,getcatagories,registerInterestwithid}= require('../controllers/projectcontrollers');
+  ,getcatagories,registerInterestwithid,acceptinterest,getcatprojects}= require('../controllers/projectcontrollers');
 const router = express.Router();
 const { ensureAuthenticated } = require('../middleware/authMiddleware');
+const { registerInterest } = require('../services/projectservice');
 
 
 
 router.post('/registerinterest/:id',ensureAuthenticated, registerInterestwithid);
 
-router.get('/',ensureAuthenticated, getProjects);
+router.get('/',ensureAuthenticated, getcatagories);
 router.get('/addproject', ensureAuthenticated,newproject);
 router.get("/mycollabration",ensureAuthenticated,getmycollabration)
 router.post('/addproject', ensureAuthenticated,createProject);
 router.get("/projectcatagories",ensureAuthenticated,getcatagories);
-router.get('/searchproject', ensureAuthenticated,searchProjects);
 
 
+router.post("/acceptinterest/:projectid/:userid", ensureAuthenticated, acceptinterest);
+router.post("/projects/registerinterest/:id", ensureAuthenticated,registerInterestwithid);
 router.post("/searchproject/:text",ensureAuthenticated,searchResults);
 router.get("/projects/:id",ensureAuthenticated,getProjectById);
 router.get("/searchresult/:text",ensureAuthenticated,getsearchresult);
@@ -29,6 +31,9 @@ router.get("/myprojects/:id",ensureAuthenticated,getmyprojectwithid)
 router.get("/followingprojects",ensureAuthenticated,getFollowingProjects );
 router.post("/delete/:id",ensureAuthenticated,deleteproject)
 
+router.get('/searchproject', ensureAuthenticated,searchProjects);
+
+router.get("/:name",ensureAuthenticated,getcatprojects);
 
 module.exports = router;
 
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,13 +1,35 @@
 const express = require('express');
+const { getUsers, contactUser, getProfile, editProfile, editProfileChange,deleteUser,getsearchresult,
+  getOtherUserProfile,renderabout,SendCodeEmail,getcurrentuser,getMyProfile,otherusers,
+  getMyFollowers,getMyFollwing,followUser,checkfollow,rendercontact,removefollower,unfollow,explore,
+getFollowers,getFollowing,delete_photo,
+deleteaccount} = require('../controllers/usercontrollers');
+const { ensureAuthenticated } = require('../middleware/authMiddleware');
+const { unFollowUser } = require('../controllers/usercontrollers');
 const router = express.Router();
 
+
+
+
+// User Routes
 
 router.get('/', ensureAuthenticated,getUsers);
 router.get("/contact",ensureAuthenticated,rendercontact);
 router.get("/about",ensureAuthenticated,renderabout)
-router.get("/myprofile",ensureAuthenticated,getMyProfile);
+router.get("/myprofile",ensureAuthenticated,getMyProfile)
+router.post("/removefollower/:id",ensureAuthenticated,removefollower)
+
 router.get("/explore",ensureAuthenticated,explore)
 router.post("/contact", contactUser);
+router.post("/follow/:id", ensureAuthenticated,followUser);
+router.post("/unfollowuser/:id",ensureAuthenticated,unfollow)
+
+
+router.get("/about",renderabout)
+router.get("/profile",ensureAuthenticated, getProfile);
+router.get("/editprofile",ensureAuthenticated, editProfile);
+router.post("/editprofile",ensureAuthenticated, editProfileChange);
+router.get("/deleteaccount",ensureAuthenticated,deleteaccount);
 router.get("/otherusers",ensureAuthenticated,getUsers)
 router.post("/deleteaccount",ensureAuthenticated,deleteUser);
 router.get("/otherprofile/:id",ensureAuthenticated, getOtherUserProfile);
@@ -16,8 +38,20 @@ router.get("/myfollowing",ensureAuthenticated,getMyFollwing );
 router.get("/followers/:id",ensureAuthenticated,getFollowers);
 router.get("/following/:id",ensureAuthenticated,getFollowing);
 router.get("/searchuser/:text",ensureAuthenticated,getsearchresult);
-router.post("/follow/:id", ensureAuthenticated,followUser);
-router.post("/unfollowuser/:id",ensureAuthenticated,unfollow)
+
+router.post("/checkfollowing/:id",ensureAuthenticated,checkfollow)
+
+router.get("/forgetpassword",ensureAuthenticated,getcurrentuser);
+router.get("/users",ensureAuthenticated,otherusers)
+router.post("/forgetpassword",ensureAuthenticated,SendCodeEmail)
+
+router.post("/checkfollow/:id", checkfollow);
+
+
+router.post("/deleteprofile",ensureAuthenticated,delete_photo)
+
+
 
 
 module.exports = router;
+
